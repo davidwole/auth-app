d2035dfa5be2c0b26110f50e6a91a07e985d76b0
davidwole <davidwole20@gmail.com>
2022-10-21 11:12:46 +0100

    Create index.js

--- index.js
+++ index.js
@@ -17,6 +17,8 @@ mongoose.connect(db).then(() => {
 });
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: false}));
+
 app.use(cors());
 app.use((req, res, next) => {
   console.log(req.path, req.method, req.ip);
