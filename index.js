99146305f15af6ac0b7fbae8fdb7e24f8dd25371
davidwole <davidwole20@gmail.com>
2022-10-21 10:52:40 +0100

    Added express body parser

--- index.js
+++ index.js
@@ -16,6 +16,8 @@ mongoose.connect(db).then(() => {
   console.log({ error: err.message})
 });
 
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
 app.use(cors());
 app.use((req, res, next) => {
   console.log(req.path, req.method, req.ip);
